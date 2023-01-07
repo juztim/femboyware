cdbf28473a27ffc1388d90067a3c352bbebaa6b6
Tim Wüstenhagen <personal@juztim.dev>
2023-01-07 07:06:08 +0100

    move get_interface_func to utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,24 @@
+use crate::trace;
+use crate::GetModuleHandleW;
+use crate::GetProcAddress;
+use std::{ffi::CStr, ptr};
+use windows::core::{s, HSTRING};
+
 pub mod math;
+
+pub unsafe fn get_interface<T>(name: &CStr, library: HSTRING) -> *mut T
+{
+    let handle = GetModuleHandleW(&library).expect("failed to get module handle");
+
+    trace!("handle: {handle:?}");
+
+    let function_address =
+        GetProcAddress(handle, s!("CreateInterface")).expect("failed to get function address");
+
+    trace!("function address: {:p}", &function_address);
+
+    type CreateInterfaceFn = extern "C" fn(*const i8, *const i32) -> usize;
+    let create_interface: CreateInterfaceFn = std::mem::transmute(function_address);
+
+    create_interface(name.as_ptr(), ptr::null()) as *mut T
+}
