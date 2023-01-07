a5fa2007202afb1f8043548d9ed81ec5f6e04113
Tim Wüstenhagen <personal@juztim.dev>
2023-01-07 07:04:52 +0100

    Add uninject functionality (VK_END)

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,20 @@ mod utils;
 #[cfg(not(target_os = "windows"))]
 compile_error!("use windows you fucking degen");
 
-use std::ffi::{c_void, CStr};
-use std::ptr;
-use simplelog::{Config, TerminalMode, ColorChoice, TermLogger};
-use log::{trace, info};
+use log::{info, trace};
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+use std::ffi::c_void;
+use windows::Win32::UI::Input::KeyboardAndMouse::GetAsyncKeyState;
 use windows::{
-    core::HSTRING,
-    s, w,
+    w,
     Win32::{
         Foundation::{BOOLEAN, HINSTANCE},
         System::{
-            Console::{AllocConsole, SetConsoleTitleW},
-            LibraryLoader::{DisableThreadLibraryCalls, GetModuleHandleW, GetProcAddress},
+            Console::{AllocConsole, FreeConsole, SetConsoleTitleW},
+            LibraryLoader::{
+                DisableThreadLibraryCalls, FreeLibraryAndExitThread, GetModuleHandleW,
+                GetProcAddress,
+            },
             SystemServices::DLL_PROCESS_ATTACH,
             Threading::{CreateThread, THREAD_CREATION_FLAGS},
         },
@@ -47,14 +49,22 @@ unsafe extern "system" fn dll_main(_lparam: *mut c_void) -> u32
 {
     AllocConsole();
     SetConsoleTitleW(w!("femboyware"));
-    
+
     log_init();
-    
+
     info!("the gay bomb has been deployed");
 
     hooks::create_move::init();
 
-    0
+    loop
+    {
+        if GetAsyncKeyState(35) != 0
+        {
+            FreeConsole();
+            FreeLibraryAndExitThread(std::mem::transmute::<*mut c_void, HINSTANCE>(_lparam), 0);
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 }
 
 #[allow(non_snake_case)]
@@ -79,12 +89,19 @@ pub unsafe extern "stdcall" fn DllMain(module: HINSTANCE, reason: u32, _: usize)
             .unwrap();
         }
 
-        _ => ()
+        _ => (),
     }
 
     true.into()
 }
 
-fn log_init() {
-    TermLogger::init(log::LevelFilter::Trace, Config::default(), TerminalMode::Stdout, ColorChoice::Always).unwrap();
+fn log_init()
+{
+    TermLogger::init(
+        log::LevelFilter::Trace,
+        Config::default(),
+        TerminalMode::Stdout,
+        ColorChoice::Always,
+    )
+    .unwrap();
 }
